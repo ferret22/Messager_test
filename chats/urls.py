95ed5c23c79873da53782dc6efb972a653ddfe05
ferret22 <avramprestolov2345@gmail.com>
2026-06-11 20:29:09 +0700

    API для истории сообщений

--- chats/urls.py
+++ chats/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ChatListAPIView
+from .views import ChatListAPIView, MessageListAPIView
 
 
 urlpatterns = [
     path('chats/', ChatListAPIView.as_view(), name='chat-list'),
+    path('chats/<int:chat_id>/messages/', MessageListAPIView.as_view(), name='message-list'),
 ]
--- chats/views.py
+++ chats/views.py
@@ -2,8 +2,9 @@
 from rest_framework import permissions
 from rest_framework.generics import ListAPIView
 
-from .models import Chat
-from .serializers import ChatSerializer
+from .models import Chat, Message
+from .serializers import ChatSerializer, MessageSerializer
+
 
 # Create your views here.
 class ChatListAPIView(ListAPIView):
@@ -14,3 +15,15 @@ class ChatListAPIView(ListAPIView):
         return Chat.objects.filter(
             members__user=self.request.user,
         ).distinct()
+
+
+class MessageListAPIView(ListAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    
+    def get_queryset(self):
+        chat_id = self.kwargs['chat_id']
+        return Message.objects.filter(
+            chat_id=chat_id,
+            chat__members__user=self.request.user,
+        ).select_related('sender')
